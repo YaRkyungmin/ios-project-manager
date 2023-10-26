5f3a766526a6ef7e2ba7c106639c624c5c9463c6
YaRkyungmin <kkgm94@naver.com>
2023-10-26 18:16:23 +0900

    ✨feat: willConnetTo 메서드 수정

--- ProjectManager/ProjectManager/App/SceneDelegate.swift
+++ ProjectManager/ProjectManager/App/SceneDelegate.swift
@@ -10,9 +10,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let UIWindowScene = (scene as? UIWindowScene) else {
+        guard let windowScene = scene as? UIWindowScene else {
             return
         }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
